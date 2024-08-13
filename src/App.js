d2a340ac585bd74dedf5604a26cd6c31116cfa89
Achanta28 <120081344+Achanta28@users.noreply.github.com>
2024-08-13 13:51:17 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,45 @@
+import logo from './logo.svg';
+import './App.css';
+import { useState } from 'react';
+import Dog from './classes/Dog';
+import Cat from './classes/Cat';
+import Animal from './classes/Animal';
+
+
+function App() {
+  let myAnimal =new Animal()
+  myAnimal.sound();
+  
+  let myDog =new Dog("Max","Golden Retriver",3,"Golden");
+  let myCat =new Cat("Whiskers","Siamese",2,"Grayy")
+  myDog.sound();
+  myDog.displayInfo();
+  myDog.eat();
+  myDog.sleep();
+  myDog.play();
+  myDog.vaccinate();
+  myCat.sound();
+  myCat.displayInfo();
+  myCat.eat();
+  myCat.scratch();
+  myCat.sleep();
+  myCat.groom();
+  myDog.calResult();
+
+
+  return(
+    <div>
+      
+
+    </div>
+
+  );
+  
+       
+            
+          
+   
+  
+}
+
+export default App;
